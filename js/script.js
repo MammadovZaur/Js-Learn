4a0367a78814f9bb66b9c26f31cefbabc4666c29
Zaur <git@erp-bs.az>
2022-06-27 18:01:56 +0400

    Start
    22.06.27-00

---
+++ js/script.js
@@ -0,0 +1,228 @@
+"use strict";
+
+// let a = 10;
+// console.log(a);
+
+// console.log("Həmd yalnız Allaha məxsusdur.");
+// let number = 5;
+// const leftBorderRadius = 1;
+
+
+// let num = 10;
+// console.log(4/0);
+// console.log("aa" * num);
+
+
+// const obj = {
+//     name: "Zaur",
+//     age: 35,
+//     avto: "Prado"
+// };
+// console.log(obj.avto);
+
+
+// alert("Salam");
+
+
+// const res = confirm("Hazirsan");
+// console.log(res);
+
+// const answer = prompt("Yash", "18");
+// console.log(answer);
+// console.log(typeof(answer));
+
+// const answers = [];
+
+// answers[0] = prompt("Adınız:", "");
+// answers[1] = prompt("Soyadınız:", "");
+// answers[2] = prompt("Yaşınız:", "");
+
+// document.write(answers);
+
+// const category = "toys";
+// console.log(`http://erp-bs.az/${category}`);
+
+
+
+// let incr = 10,
+//     decr = 10;
+
+
+// incr++;
+// decr--;
+
+// console.log(++incr);
+// console.log(--decr);
+
+// const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
+
+
+
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
+
+
+// const answers01 = prompt("Один из последних просмотренных фильмов?", "");
+// const answers02 = prompt("На сколько оцените его?", "");
+
+
+// const  movies = {
+//     'logan': '8.1'
+// };
+
+// personalMovieDB.movies[answers01] = answers02;
+// personalMovieDB.movies[answers01] = answers02;
+
+// console.log(personalMovieDB);
+
+// const num = 12;
+
+// if(num === 10) {
+//     console.log("10");
+// }else if (num === 12) {
+//     console.log("12");
+// }else {
+//     console.log("Duz deyil!");
+// }
+
+// const num = 12;
+
+// (num ===12) ? console.log("OK"): console.log("NO");
+
+
+
+// const num = 50;
+// switch (num) {
+//     case 2:
+//         console.log(2);
+//         break;
+//     case 15:
+//         console.log(15);
+//         break;
+//     case 30:
+//         console.log(30);
+//         break;
+//     case 50:
+//         console.log(50);
+//         break;
+//     case 40:
+//         console.log(40);
+//         break;
+//     default:
+//         console.log(4);
+//         break;
+// }
+
+
+
+// console.log(5 === 3 || 5<7 );
+
+
+
+// let num = 50;
+
+// while (num <= 55){
+//     console.log(num);
+//     num = num + 1;
+// }
+
+// let num = 50;
+// do{
+//     console.log(num);
+//     num = num + 1;
+// }while(num <= 55);
+
+
+
+
+// for(let i =1; i<8;){
+//     console.log(i);
+//     i++;
+// }
+
+// for (let i = 1; i < 8; i++) {
+//     if (i ===5) {
+//         // break;
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+// let result = "";
+// const leng = 7;
+
+// for(let i = 0; i < leng; i++){
+
+//     for(let j = 0; j<i; j++){
+//         result += "*";
+//     }
+
+//     result += "\n";
+// }
+// console.log(result);
+
+
+// first: for(let i = 1; i < 5; i++ ){
+//     console.log("Fist level: "+i);
+//     for(let j = 1; j < 5; j++ ){
+//         console.log("Second level: "+j);
+//         for(let k = 1; k < 5; k++ ){
+//             if(k===2) {break first;}
+//             console.log("Third level: "+k);
+//         }
+//     }
+// }
+
+
+
+
+
+
+// //****************************Home work 2 *************************\\
+
+// const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
+
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
+
+
+// // const question = prompt("Один из последних просмотренных фильмов?", "");
+// // const answer  = prompt("На сколько оцените его?", "");
+
+// const quantity = 2;
+// for (let i = 1; i <= quantity; i++) {
+//     const question = prompt("Один из последних просмотренных фильмов?", ""),
+//           answer = prompt("На сколько оцените его?", "");
+
+//     if (question != '' && question != null && answer != '' && answer != null && answer.length <50 ) {
+//         personalMovieDB.movies[question] = answer;
+//     } else {
+//         alert("Sual və ya cavab boş ola bilməz!!!");
+//         i--;
+//     }
+// }
+
+// if (personalMovieDB.count < 10){
+//     console.log("Просмотрено довольно мало фильмов");
+// }else if(personalMovieDB.count > 10 && personalMovieDB.count < 30 ){
+//     console.log("Вы классический зритель");
+// }else if(personalMovieDB.count > 30){
+//     console.log("Вы киноман");
+// }else{
+//     console.log("Произошла ошибка");
+// }
+
+// console.log(personalMovieDB);
+
+// //****************************Home work 2 *************************\\
+
